39450a02ad6aa7c93d57fa15ce201295ffb0bab0
Jay Mark Estrera <estrerajaymark@ymail.com>
2012-07-06 15:35:58 +0800

    Started Bindings

---
+++ DocNanzDCMS/CustomTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocNanzDCMS
+{
+    public class CustomTextBox
+    {
+        private String text = "Hello";
+        public String Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
