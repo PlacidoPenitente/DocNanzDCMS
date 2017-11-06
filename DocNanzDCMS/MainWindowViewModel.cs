cac01112a38e1d8c9c959fd9a60fa6ff083ac940
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-11-06 13:24:00 +0800

    Menu Changes and Added ViewModel on MainWindow

---
+++ DocNanzDCMS/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DocNanzDCMS
+{
+    public class MainWindowViewModel
+    {
+        private String title;
+        public String Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public MainWindowViewModel()
+        {
+            title = "Doc Nanz | Dental Clinic";
+        }
+    }
+}
