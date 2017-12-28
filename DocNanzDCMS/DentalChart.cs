61043d775342d9908898342cccb46a9464200be3
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-28 21:20:46 +0800

    Set Tooth No.

--- DocNanzDCMS/DentalChart.cs
+++ DocNanzDCMS/DentalChart.cs
@@ -69,6 +69,66 @@ namespace DocNanzDCMS
 
         public DentalChart()
         {
+            tooth11.ToothNo = "11";
+            tooth12.ToothNo = "12";
+            tooth13.ToothNo = "13";
+            tooth14.ToothNo = "14";
+            tooth15.ToothNo = "15";
+            tooth16.ToothNo = "16";
+            tooth17.ToothNo = "17";
+            tooth18.ToothNo = "18";
+
+            tooth21.ToothNo = "21";
+            tooth22.ToothNo = "22";
+            tooth23.ToothNo = "23";
+            tooth24.ToothNo = "24";
+            tooth25.ToothNo = "25";
+            tooth26.ToothNo = "26";
+            tooth27.ToothNo = "27";
+            tooth28.ToothNo = "28";
+
+            tooth31.ToothNo = "31";
+            tooth32.ToothNo = "32";
+            tooth33.ToothNo = "33";
+            tooth34.ToothNo = "34";
+            tooth35.ToothNo = "35";
+            tooth36.ToothNo = "36";
+            tooth37.ToothNo = "37";
+            tooth38.ToothNo = "38";
+
+            tooth41.ToothNo = "41";
+            tooth42.ToothNo = "42";
+            tooth43.ToothNo = "43";
+            tooth44.ToothNo = "44";
+            tooth45.ToothNo = "45";
+            tooth46.ToothNo = "46";
+            tooth47.ToothNo = "47";
+            tooth48.ToothNo = "48";
+
+            tooth51.ToothNo = "51";
+            tooth52.ToothNo = "52";
+            tooth53.ToothNo = "53";
+            tooth54.ToothNo = "54";
+            tooth55.ToothNo = "55";
+
+            tooth61.ToothNo = "61";
+            tooth62.ToothNo = "62";
+            tooth63.ToothNo = "63";
+            tooth64.ToothNo = "64";
+            tooth65.ToothNo = "65";
+
+            tooth71.ToothNo = "71";
+            tooth72.ToothNo = "72";
+            tooth73.ToothNo = "73";
+            tooth74.ToothNo = "74";
+            tooth75.ToothNo = "75";
+
+            tooth81.ToothNo = "81";
+            tooth82.ToothNo = "82";
+            tooth83.ToothNo = "83";
+            tooth84.ToothNo = "84";
+            tooth85.ToothNo = "85";
+
             tooth11.Image = "";
             tooth12.Image = "";
             tooth13.Image = "";
