05dd6612603e70791eaf52af205da2b1a6b96ae7
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-28 20:49:19 +0800

    Removed Restorations.

--- DocNanzDCMS/Tooth.cs
+++ DocNanzDCMS/Tooth.cs
@@ -9,8 +9,10 @@ namespace DocNanzDCMS
     {
         private string toothNo;
         private string image;
-
         private List<Procedure> procedures = new List<Procedure>();
+
+        public string ToothNo { get => toothNo; set => toothNo = value; }
         public string Image { get => image; set => image = value; }
+        public List<Procedure> Procedures { get => procedures; set => procedures = value; }
     }
 }
