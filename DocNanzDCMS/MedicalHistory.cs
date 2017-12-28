d942e2a653041c0d0bdd41ca7434ec2bd2abae76
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-28 19:45:35 +0800

    Revised Patient and Medical Model.

--- DocNanzDCMS/MedicalHistory.cs
+++ DocNanzDCMS/MedicalHistory.cs
@@ -7,8 +7,42 @@ namespace DocNanzDCMS
 {
     public class MedicalHistory
     {
-        private DateTime evaluationDate;
+        private Patient patient;
+        private string physicianName = "Yedda Marie Lingad";
+        private string physicianSpecialty = "None";
+        private string physicianAddress = "Mandaluyong City";
+        private string physicianNo = "12345";
+        private bool isInGoodHealth = true;
+        private string medicalTreatment = "None";
+        private string seriousIllnessOrOperation = "None";
+        private string hospitalizationReason = "None";
+        private string medicationTaken = "None";
+        private bool isTobaccoUser = false;
+        private bool isDangerousDrugsUser = false;
+        private List<string> medicinesAllergicTo = new List<string>() {"Antibiotic", "Penicillin"};
+        private string bleedingTime = "1";
+        private bool isPregnant = false;
+        private string bloodType = "A";
+        private string bloodPressure = "100/70";
+        private List<string> diseases = new List<string>() {"High Blood Pressure", "Diabetes"};
 
-        public DateTime EvaluationDate { get => evaluationDate; set => evaluationDate = value; }
+        public Patient Patient { get => patient; set => patient = value; }
+        public string PhysicianName { get => physicianName; set => physicianName = value; }
+        public string PhysicianSpecialty { get => physicianSpecialty; set => physicianSpecialty = value; }
+        public string PhysicianAddress { get => physicianAddress; set => physicianAddress = value; }
+        public string PhysicianNo { get => physicianNo; set => physicianNo = value; }
+        public bool IsInGoodHealth { get => isInGoodHealth; set => isInGoodHealth = value; }
+        public string MedicalTreatment { get => medicalTreatment; set => medicalTreatment = value; }
+        public string SeriousIllnessOrOperation { get => seriousIllnessOrOperation; set => seriousIllnessOrOperation = value; }
+        public string HospitalizationReason { get => hospitalizationReason; set => hospitalizationReason = value; }
+        public string MedicationTaken { get => medicationTaken; set => medicationTaken = value; }
+        public bool IsTobaccoUser { get => isTobaccoUser; set => isTobaccoUser = value; }
+        public bool IsDangerousDrugsUser { get => isDangerousDrugsUser; set => isDangerousDrugsUser = value; }
+        public List<string> MedicinesAllergicTo { get => medicinesAllergicTo; set => medicinesAllergicTo = value; }
+        public string BleedingTime { get => bleedingTime; set => bleedingTime = value; }
+        public bool IsPregnant { get => isPregnant; set => isPregnant = value; }
+        public string BloodType { get => bloodType; set => bloodType = value; }
+        public string BloodPressure { get => bloodPressure; set => bloodPressure = value; }
+        public List<string> Diseases { get => diseases; set => diseases = value; }
     }
 }
--- DocNanzDCMS/Patient.cs
+++ DocNanzDCMS/Patient.cs
@@ -7,30 +7,34 @@ namespace DocNanzDCMS
 {
     public class Patient : Person
     {
-        private string religion;
-        private string nationality;
-        private string occupation;
-        private Patient father;
-        private Patient mother;
-        private Patient guardian;
-        private List<Patient> children;
-        private List<Patient> siblings;
-        private MedicalHistory medicalRecord;
-        private DentalChart dentalChart;
-        private Transaction transaction;
-        private Operation operation;
+        private string religion = "Roman Catholic";
+        private string nationality = "Filipino";
+        private string nickname = "Potenciano";
+        private string occupation = "none";
+        private string officeno = "12345";
+        private string insurance = "none";
+        private DateTime insuranceDate = DateTime.Now;
+        private string faxno = "12345";
+        private string guardianName = "Daisy Estrera";
+        private string guardianOccupation = "None";
+        private string referree = "None";
+        private string consultationReason = "Toothache";
+        private string previousDentist = "Dr. Estrera";
+        private DateTime lastDentalVisit = DateTime.Now;
 
         public string Religion { get => religion; set => religion = value; }
         public string Nationality { get => nationality; set => nationality = value; }
+        public string Nickname { get => nickname; set => nickname = value; }
         public string Occupation { get => occupation; set => occupation = value; }
-        public Patient Father { get => father; set => father = value; }
-        public Patient Mother { get => mother; set => mother = value; }
-        public Patient Guardian { get => guardian; set => guardian = value; }
-        public List<Patient> Children { get => children; set => children = value; }
-        public List<Patient> Siblings { get => siblings; set => siblings = value; }
-        public MedicalHistory MedicalRecord { get => medicalRecord; set => medicalRecord = value; }
-        public DentalChart DentalChart { get => dentalChart; set => dentalChart = value; }
-        public Transaction Transaction { get => transaction; set => transaction = value; }
-        public Operation Operation { get => operation; set => operation = value; }
+        public string Officeno { get => officeno; set => officeno = value; }
+        public string Insurance { get => insurance; set => insurance = value; }
+        public DateTime InsuranceDate { get => insuranceDate; set => insuranceDate = value; }
+        public string Faxno { get => faxno; set => faxno = value; }
+        public string GuardianName { get => guardianName; set => guardianName = value; }
+        public string GuardianOccupation { get => guardianOccupation; set => guardianOccupation = value; }
+        public string Referree { get => referree; set => referree = value; }
+        public string ConsultationReason { get => consultationReason; set => consultationReason = value; }
+        public string PreviousDentist { get => previousDentist; set => previousDentist = value; }
+        public DateTime LastDentalVisit { get => lastDentalVisit; set => lastDentalVisit = value; }
     }
 }
