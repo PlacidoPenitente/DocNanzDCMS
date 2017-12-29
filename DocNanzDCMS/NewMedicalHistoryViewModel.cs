48d7f8da9b926ea04b0cc06651f836087057ec9e
Jay Mark Estrera <estrerajaymark@ymail.com>
2017-12-29 14:59:33 +0800

    Implemented INotifyPropertyChanged.

--- DocNanzDCMS/NewMedicalHistoryViewModel.cs
+++ DocNanzDCMS/NewMedicalHistoryViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
 namespace DocNanzDCMS
 {
-    public class NewMedicalHistoryViewModel
+    public class NewMedicalHistoryViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            Console.WriteLine(propertyName);
+        }
     }
 }
